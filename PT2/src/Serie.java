f47017d06e9cc63f83a9c8db21386f4f7f409bd1
Marc Pellicer <marcpelli3@gmail.com>
2023-10-08 20:51:36 +0200

    added feature serie

---
+++ PT2/src/Serie.java
@@ -0,0 +1,74 @@
+
+public class Serie {
+
+	public String titulo;
+	public int temporadas;
+	public boolean entregado;
+	public String genero;
+	public String creador;
+	
+	public Serie() {
+		
+		this.titulo = "";
+		this.temporadas  = 3;
+		this.entregado = false;
+		this.genero = "";
+		this.creador = "";
+		
+	}
+	
+	public Serie(String titulo, int temporadas, boolean entregado, String genero, String creador) {
+		
+		this.titulo = titulo;
+		this.temporadas = temporadas;
+		this.entregado = entregado;
+		this.genero = genero;
+		this.creador = creador;
+	}
+	
+	Serie defecto = new Serie();
+	Serie dark = new Serie("Dark", temporadas, entregado, genero, "Random");
+	Serie darki = new Serie("Dark", 5, entregado, "Drama", "Random");
+	
+	
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public int getTemporadas() {
+		return temporadas;
+	}
+
+	public void setTemporadas(int temporadas) {
+		this.temporadas = temporadas;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getCreador() {
+		return creador;
+	}
+
+	public void setCreador(String creador) {
+		this.creador = creador;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Serie [titulo= " + titulo + ", temporadas= " + temporadas + ", entregado= " + entregado + ", genero= "
+				+ genero + ", creador= " + creador + "]";
+	}
+
+}
+
